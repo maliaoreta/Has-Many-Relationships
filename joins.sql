ec32ff7ff054331468aee83bbdc6618214bef464
maliaoreta <maliaoreta@gmail.com>
2016-04-19 18:09:18 -1000

    query to get all post fields and username when user first name is norene & last name is schmitt

--- joins.sql
+++ joins.sql
@@ -9,13 +9,17 @@
 -- WHERE users_id = 100;
 
 -- Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
-SELECT posts.*, users.first_name, users.last_name FROM posts
-INNER JOIN users
-ON users.id = posts.users_id
-WHERE users_id = 200;
+-- SELECT posts.*, users.first_name, users.last_name FROM posts
+-- INNER JOIN users
+-- ON users.id = posts.users_id
+-- WHERE users_id = 200;
 
 -- Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
-
+-- SELECT posts.*, users.username FROM posts
+-- INNER JOIN users
+-- ON users.id = posts.users_id
+-- WHERE users.first_name = 'Norene'
+-- AND users.last_name = 'Schmitt';
 
 -- Create a query to get usernames from the users table where the user has created a post after January 1, 2015
 
