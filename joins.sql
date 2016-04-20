e4e9dd38bd558c6ebf319ae3ae701c12dc0343a5
maliaoreta <maliaoreta@gmail.com>
2016-04-19 18:05:10 -1000

    write query that gets all post fields, users first & last name from posts table where users id is 200

--- joins.sql
+++ joins.sql
@@ -5,11 +5,14 @@
 -- SELECT * FROM users;
 
 -- Create a query to get all fields from the posts table where the user_id is 100
-SELECT * FROM posts
-WHERE users_id = 100;
+-- SELECT * FROM posts
+-- WHERE users_id = 100;
 
 -- Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
-
+SELECT posts.*, users.first_name, users.last_name FROM posts
+INNER JOIN users
+ON users.id = posts.users_id
+WHERE users_id = 200;
 
 -- Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
 
